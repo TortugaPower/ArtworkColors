56875394ea63b49d90e15e6f5fb432f13882e5ee
Florian Pichler <fp@ylk.gd>
2019-02-19 17:18:35 +0100

    Initial Commit

---
+++ ArtworkExtract/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ArtworkExtract
+//
+//  Created by Florian Pichler on 19.02.19.
+//  Copyright © 2019 Florian Pichler. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ArtworkExtract/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ArtworkExtract
+//
+//  Created by Florian Pichler on 19.02.19.
+//  Copyright © 2019 Florian Pichler. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
